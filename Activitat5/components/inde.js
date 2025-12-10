37fe7e81a33c7c090b86b4cac2da856634fcafcd
cincastello <cintacastello@iesebre.com>
2025-12-10 18:26:44 +0100

    Update inde.js

--- Activitat5/components/inde.js
+++ Activitat5/components/inde.js
@@ -2,3 +2,19 @@ function fotogran(foto){
     document.getElementById('fotogran').src=foto
     document.getElementById('fotogran').style.visibility='visible'
 }
+function ensenya() {
+    const seccio = document.getElementById("seccio");
+    const text = document.getElementById("textOcult");
+
+    seccio.style.backgroundColor = "darkpink";
+    seccio.style.color = "white";
+    text.style.display = "block";
+}
+function restaura() {
+    const seccio = document.getElementById("seccio");
+    const text = document.getElementById("textOcult");
+
+    seccio.style.backgroundColor = "lavender";
+    seccio.style.color = "black";
+    text.style.display = "none";
+}
